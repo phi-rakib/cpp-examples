d2031399345cca6668d00594da1f0c09786a682a
rakib <phi.rakib@gmail.com>
2021-06-25 22:25:43 +0600

    add items to the map

--- containers/map.cpp
+++ containers/map.cpp
@@ -6,5 +6,10 @@ int main() {
   // declare map
   map<string, int> mp;
   
+  // add items to the map
+  mp.insert(pair<string, int>("jakir", 22));
+  mp.insert(pair<string, int>("abir", 17));
+  mp.insert(pair<string, int>("raihan", 45));
+  
   return 0;
 }
