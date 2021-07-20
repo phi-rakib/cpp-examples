64d1b38b52e9985e0fb18d2df4651d5014e19edd
rakib <phi.rakib@gmail.com>
2021-07-20 22:40:25 +0600

    added example of struct address

---
+++ struct/4-struct-address.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+// create employee struct
+struct Employee {
+  int id;
+  char name[50];
+};
+
+int main() 
+{
+  // declare and initialize employee struct 
+  struct Employee employee = {122, "Jacob"};
+
+  // get the memory address of employee
+  printf("%d\n", &employee);
+
+  // get the memory address of employee id & compare with employee address
+  printf("%d\n", &employee.id);
+
+  // get the memory address of employee name
+  printf("%d\n", &employee.name);
+}
